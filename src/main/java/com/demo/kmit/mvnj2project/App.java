4bf9cf7266b621622e953164636ef3ff37a2e579
Haleema Bushra <cutebushra8@gmail.com>
2024-11-15 14:25:44 +0530

    mvnj2project added

---
+++ src/main/java/com/demo/kmit/mvnj2project/App.java
@@ -0,0 +1,13 @@
+package com.demo.kmit.mvnj2project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
